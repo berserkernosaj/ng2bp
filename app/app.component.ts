53caa9123d40fac4c0940725dd9092ed98d36c5c
Luis Hernandez <luishrd@users.noreply.github.com>
2016-04-25 13:39:53 -0600

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector: 'ng-app',
-	template: '<h3>Application Component</h3>'
+	template: '<h3>Application Loaded Successfully</h3>'
 })
 export class AppComponent {
 	
